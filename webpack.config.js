12d106ea942fc40e6e9ae7995df00e1774d77ee9
Piotr Bechcicki <piotr.bechcicki@codilime.com>
2019-06-01 01:37:14 +0200

    It is possible to bundle the files now.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,51 @@
+const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const glob = require('glob')
 
 module.exports = {
-  entry: {
-    'bundle.js': glob.sync('build/static/?(js|css)/*.?(js|css)').map((f) => path.resolve(__dirname, f)),
-  },
+  entry: [
+    '@babel/polyfill',
+    './src/index.js',
+  ],
   output: {
-    filename: 'build/static/js/bundle.min.js',
+    filename: 'bundle.min.js',
+    publicPath: '/collab/',
   },
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+
+      // images
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]',
+          },
+        }],
+      },
+      //File loader used to load fonts
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
     ],
   },
-  plugins: [new UglifyJsPlugin()],
+  optimization: {
+    minimizer: [new TerserPlugin()],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, './src'), 'node_modules'],
+  },
 }
